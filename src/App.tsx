dfc72c4458fd916324e16614fc4e4e2299c18be2
🐸 <dice-a6db@outlook.com>
2025-01-09 18:17:58 +0000

    chore: add biome

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { HelloWorld } from "../dist/main";
 import "../dist/main.css";
 
 export function App() {
-  return (
-    <div>
-      <HelloWorld />
-    </div>
-  );
+	return (
+		<div>
+			<HelloWorld />
+		</div>
+	);
 }
